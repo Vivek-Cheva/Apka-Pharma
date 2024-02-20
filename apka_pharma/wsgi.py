eb67f5209a2dace04ea0458b30bc3b147f819369
Vivek Cheva <vivek.cheva@gmail.com>
2024-02-20 23:01:36 +0530

    updated for deployment

--- apka_pharma/wsgi.py
+++ apka_pharma/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apka_pharma.settings')
-
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apka_pharma.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "apka_pharma.settings"
 application = get_wsgi_application()
 
-# app = application
+app = application
